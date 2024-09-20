21fb10eac57ced71f90711731b1134952a1f65eb
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2024-09-20 00:42:32 -0300

    feat: fix some problems with color of text

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,24 +1,36 @@
 // src/components/ExpenseList.jsx
 import React from 'react';
-import { List, ListItem, ListItemText, ListItemSecondaryAction, Button } from '@mui/material';
+import { List, ListItem, ListItemText, ListItemSecondaryAction, Button, Typography } from '@mui/material';
 
 const ExpenseList = ({ expenses, onRemoveExpense }) => {
+  const total = expenses.reduce((acc, expense) => acc + parseFloat(expense.value), 0);
+
   return (
-    <List>
-      {expenses.map((expense, index) => (
-        <ListItem key={index}>
-          <ListItemText
-            primary={`${expense.name} - R$ ${expense.value}`}
-            secondary={`Data: ${expense.date}`}
-          />
-          <ListItemSecondaryAction>
-            <Button variant="outlined" color="secondary" onClick={() => onRemoveExpense(index)}>
-              Remover
-            </Button>
-          </ListItemSecondaryAction>
-        </ListItem>
-      ))}
-    </List>
+    <div>
+      <List>
+        {expenses.map((expense, index) => (
+          <ListItem key={index}>
+            <ListItemText
+              primary={
+                <Typography variant="body1" sx={{ color: 'black' }}>
+                  {`${expense.name} - R$ ${expense.value}`}
+                </Typography>
+              }
+              secondary={
+                <Typography variant="body2" sx={{ color: 'black' }}>
+                  {`Data: ${expense.date}`}
+                </Typography>
+              }
+            />
+            <ListItemSecondaryAction>
+              <Button variant="outlined" color="secondary" onClick={() => onRemoveExpense(index)}>
+                Remover
+              </Button>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      </List>
+    </div>
   );
 };
 
